5a5251428e0fe72ee2280be3308318e2c996dd6f
benNjogu <kamandemachafi@gmail.com>
2023-01-28 07:08:23 +0300

    columns per row

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -21,7 +21,7 @@ const Posts = ({ setCurrentId }) => {
       spacing={3}
     >
       {posts.map((post) => (
-        <Grid key={post._id} item xs={12} sm={6}>
+        <Grid key={post._id} item lg={3} md={6} xs={12} sm={12}>
           <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
