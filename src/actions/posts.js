0d22e7624adbd867eb9007f60c6d5387acf36184
benNjogu <kamandemachafi@gmail.com>
2023-02-02 14:04:38 +0300

    Comment on posts

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -7,6 +7,7 @@ import {
   DELETE,
   START_LOADING,
   END_LOADING,
+  COMMENT,
 } from "../constants/actionTypes";
 import * as api from "../api";
 
@@ -85,6 +86,17 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
     dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const commentPost = (value, id) => async (dispatch) => {
+  try {
+    const { data } = await api.comment(value, id);
+    dispatch({ type: COMMENT, payload: data });
+
+    return data.comments;
   } catch (error) {
     console.log(error);
   }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -8,6 +8,7 @@ import {
   LIKE,
   START_LOADING,
   END_LOADING,
+  COMMENT,
 } from "../constants/actionTypes";
 
 export default (state = { isLoading: true, posts: [] }, action) => {
@@ -42,6 +43,15 @@ export default (state = { isLoading: true, posts: [] }, action) => {
         posts: state.posts.map((post) =>
           post._id === action.payload._id ? action.payload : post
         ),
+      };
+    case COMMENT:
+      return {
+        ...state,
+        posts: state.posts.map((post) => {
+          if (post._id === action.payload._id) return action.payload;
+
+          return post;
+        }),
       };
     case DELETE:
       return {
